e7b29fb7afd00c053643cac3bb650710c01b5c94
tim.aleinikov <tim.aleinikov@gmail.com>
2022-07-21 11:56:25 +0300

    add .env for variables and some fixes

---
+++ postgresql/1_create_de_user.sql
@@ -0,0 +1,18 @@
+DO
+$do$
+BEGIN
+   IF EXISTS (
+      select usename FROM pg_catalog.pg_user
+      WHERE  usename = 'de_user') THEN
+
+      RAISE NOTICE 'Role "de_user" already exists. Skipping.';
+   ELSE
+      BEGIN   -- nested block
+         CREATE USER de_user with encrypted password 'de_pass';
+      EXCEPTION
+         WHEN duplicate_object THEN
+            RAISE NOTICE 'User "de_user" was just created by a concurrent transaction. Skipping.';
+      END;
+   END IF;
+END
+$do$;

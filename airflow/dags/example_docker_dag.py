12c4cc1c07730b74c28c668e2adefa8f459d259c
tim.aleinikov <tim.aleinikov@gmail.com>
2023-06-07 10:37:36 +0600

    airflow: custom xcom: add readme and examples

--- airflow/dags/example_docker_dag.py
+++ airflow/dags/example_docker_dag.py
@@ -5,19 +5,27 @@ from datetime import datetime
 
 @dag(start_date=datetime(2022,1,1), schedule="@weekly", catchup=False)
 def docker_example_dag():
+
     @task()
-    
-    def t1():
+    def dummy_task():
         pass
 
-    t2 = DockerOperator(
-        task_id='t2',
+    docker_task = DockerOperator(
+        task_id='docker_task',
         image='python:3.10-slim-buster',
         command='echo "Execution date is {{ ds }}")',
         docker_url='unix://var/run/docker.sock',
-        network_mode='bridge'
+        network_mode='bridge',
+        auto_remove=True,
+        container_name='airflow-task-{{ ds }}',
+        environment={
+            'SOME_ENV': 'value'
+        },
+        private_environment={
+            'SECRET_ENV': 'value'
+        }
     )
 
-    t1() >> t2
+    dummy_task() >> docker_task
 
 dag = docker_example_dag()
